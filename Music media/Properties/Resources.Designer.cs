7b107002c5586542a7fa204f9027dfdce886c357
AnhkhoaBeoz <86869877+AnhkhoaBeoz@users.noreply.github.com>
2023-04-27 17:37:23 +0700

    Khoabeo2 (#10)
    * Update
    
    * 4H 50

--- Music media/Properties/Resources.Designer.cs
+++ Music media/Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace Music_media.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _285655_user_icon {
+            get {
+                object obj = ResourceManager.GetObject("285655_user_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _385755_user_male_icon {
+            get {
+                object obj = ResourceManager.GetObject("385755_user_male_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap back {
             get {
                 object obj = ResourceManager.GetObject("back", resourceCulture);
